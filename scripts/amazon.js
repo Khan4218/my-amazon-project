ac88d00235d6290cb0c96f0a719ae78fbaf21af8
Khan4218 <mirakber4@gmail.com>
2025-05-17 03:36:00 +0530

    Make cart-quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,10 +81,16 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
+      let cartQuantity = 0;
 
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
 
+      });
 
-      console.log(cart);
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity
+
 
     });
 
