79eb4046d0f676592e566db4378ba498c7d40de3
Khan4218 <mirakber4@gmail.com>
2025-05-21 06:27:15 +0530

    Create OOP version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,119 @@
+function Cart(localStorageKey) {
+  const cart = {
+    cartItems: undefined,
+
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
+
+      if (!this.cartItems) {
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+
+        }];
+      }
+    },
+
+    saveToStorage() {
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+    },
+
+
+    addToCart(productId, quantity = null) {
+      if (quantity === null) {
+        const quantitySelector = document.querySelector(`.js-product-quantity-selector-${productId} select`);
+        quantity = Number(quantitySelector?.value || 1); // Use 1 as fallback
+      }
+
+
+
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += quantity;
+      } else {
+        this.cartItems.push({
+
+          productId: productId,
+          quantity: quantity,
+          deliveryOptionId: '1'
+        });
+      }
+
+      this.saveToStorage();
+
+    },
+
+
+    removeFromCart(productId) {
+
+      const newCart = [];
+      this.cartItems.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+          newCart.push(cartItem);
+        }
+
+      });
+
+      cart.length = 0;
+      cart.push(...newCart);
+
+      this.saveToStorage();
+    },
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem;
+
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+
+      });
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+    }
+
+
+  };
+
+  return cart;
+
+}
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
+cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
