243186111d23621f9217f34b2a3dfd6c293dab57
Khan4218 <mirakber4@gmail.com>
2025-05-21 15:30:03 +0530

    load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,109 +1,113 @@
 import { cart, addToCart } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+loadProducts(renderProductsGrid);
 
-let productsHTML = '';
+function renderProductsGrid() {
 
-products.forEach((product) => {
-  productsHTML +=
+  let productsHTML = '';
 
-    `<div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image" src="${product.image}">
-          </div>
+  products.forEach((product) => {
+    productsHTML +=
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars" 
-            src="${product.getStarsUrl()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+      `<div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image" src="${product.image}">
             </div>
-          </div>
 
-          <div class="product-price">
-            ${product.getPrice()}
-          </div>
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
 
-          <div class="product-quantity-container
-          js-product-quantity-selector-${product.id}">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
+            <div class="product-rating-container">
+              <img class="product-rating-stars" 
+              src="${product.getStarsUrl()}">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
 
-          ${product.extraInfoHTML()}
+            <div class="product-price">
+              ${product.getPrice()}
+            </div>
 
-          <div class="product-spacer"></div>
+            <div class="product-quantity-container
+            js-product-quantity-selector-${product.id}">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
 
-          <div class="added-to-cart
-          js-added-to-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
+            ${product.extraInfoHTML()}
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${product.id}">
-            Add to Cart
-          </button>
-        </div>`;
-  ;
-});
+            <div class="product-spacer"></div>
 
+            <div class="added-to-cart
+            js-added-to-cart-${product.id}">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
 
-
-document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;
-
-function updateCartQuantity(productId) {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id= "${product.id}">
+              Add to Cart
+            </button>
+          </div>`;
+    ;
   });
 
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
 
-  const addedMessageTimeouts = {};
-  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
-  addedMessage.classList.add('added-to-cart-visible');
+  document.querySelector('.js-products-grid')
+    .innerHTML = productsHTML;
 
-  const previousTimeoutId = addedMessageTimeouts[productId];
+  function updateCartQuantity(productId) {
+    let cartQuantity = 0;
 
-  if (previousTimeoutId) {
-    clearTimeout(previousTimeoutId);
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+
+    });
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
+    const addedMessageTimeouts = {};
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
 
   }
 
-  const timeoutId = setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
-  }, 2000);
-
-  addedMessageTimeouts[productId] = timeoutId;
-
-}
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.
-        productId;
-      addToCart(productId);
-      updateCartQuantity(productId);
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.
+          productId;
+        addToCart(productId);
+        updateCartQuantity(productId);
+      });
     });
-  });
+}
